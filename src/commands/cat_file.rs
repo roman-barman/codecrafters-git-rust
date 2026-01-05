cb6cf77db48e12cc989da024dfe72492a7f72f79
Roman Zanko <roman.zanko.vl@gmail.com>
2026-01-05 17:34:30 +0400

    Improve error handling and refactor return types in `cat_file` and `main` functions

--- src/commands/cat_file.rs
+++ src/commands/cat_file.rs
@@ -3,27 +3,44 @@ use flate2::read::ZlibDecoder;
 use std::fs;
 use std::io::Read;
 
-pub(crate) fn cat_file(hash: &str) {
+pub(crate) fn cat_file(hash: &str) -> Result<(), CatFileError> {
     let dir_name = &hash[..2];
     let file_name = &hash[2..];
 
-    let bytes =
-        fs::read(format!("./{}/{}/{}", OBJECT_DIR, dir_name, file_name)).unwrap_or_else(|_| {
-            panic!(
-                "Failed to read file: {}/{}/{}",
-                OBJECT_DIR, dir_name, file_name
-            )
-        });
+    let bytes = fs::read(format!("./{}/{}/{}", OBJECT_DIR, dir_name, file_name)).map_err(|e| {
+        CatFileError::FailedToRead {
+            object: hash.to_string(),
+            source: e,
+        }
+    })?;
 
     let mut decoder = ZlibDecoder::new(&bytes[..]);
     let mut content = String::new();
     decoder
         .read_to_string(&mut content)
-        .expect("Failed to read zlib compressed content");
+        .map_err(|e| CatFileError::FailedToUnzip {
+            object: hash.to_string(),
+            source: e,
+        })?;
     let content_start = match content.find("\0") {
         Some(start) => start + 1,
         None => 0,
     };
 
     print!("{}", &content.as_str()[content_start..]);
+    Ok(())
+}
+
+#[derive(Debug, thiserror::Error)]
+pub(crate) enum CatFileError {
+    #[error("Failed to read object {object}: {source}")]
+    FailedToRead {
+        object: String,
+        source: std::io::Error,
+    },
+    #[error("Failed to unzip object {object}: {source}")]
+    FailedToUnzip {
+        object: String,
+        source: std::io::Error,
+    },
 }
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ mod commands;
 use crate::args::{AppArgs, Command};
 use clap::Parser;
 
-fn main() -> Result<(), anyhow::Error> {
+fn main() {
     let command = AppArgs::parse().command;
-    match command {
-        Command::Init => commands::init()?,
-        Command::CatFile(args) => commands::cat_file(&args.hash),
+    let result = match command {
+        Command::Init => commands::init().map_err(anyhow::Error::new),
+        Command::CatFile(args) => commands::cat_file(&args.hash).map_err(anyhow::Error::new),
     };
 
-    Ok(())
+    if let Err(e) = result {
+        eprintln!("{}", e);
+    }
 }
